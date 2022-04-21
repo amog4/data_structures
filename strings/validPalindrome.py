2c97eb6904cbc57ef24573a13b9b823509879ba1
amog4 <saiamoghc@gmail.com>
2022-04-21 22:08:18 +0530

    string

---
+++ strings/validPalindrome.py
@@ -0,0 +1,33 @@
+s = "Race Car"
+
+from turtle import st
+
+
+class Solution:
+    
+    def validate_palindrome(self,s):
+
+        s = str(s).lower()
+        start = 0
+        end = len(s) - 1
+
+        while start < end:
+            
+            if not s[start].isalnum() and start < end:
+                start += 1
+            if not s[end].isalnum() and start < end:
+                end -= 1
+            
+
+            if s[start] != s[end]:
+                return False
+
+            start += 1
+            end -= 1
+        
+        return True
+
+
+ss = Solution()
+
+ss.validate_palindrome(s= s)
